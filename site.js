3412fa9c0a8a97e058e42276476ab4fa969c7afa
karinakuraj <karina_gretta@ukr.net>
2016-04-07 15:29:41 +0000

    add new file

--- site.js
+++ site.js
@@ -2,9 +2,9 @@
 $(document).ready(function() {
   $('#btn1').click(function() {
      if ($('#about').css('display')=='none') {
-    $('#about').animate({left:'show'},500);
+    $('#about').animate({left:'show'},200);
   }else{
-    $('#about').animate({left:'hide'},500);
+    $('#about').animate({left:'hide'},200);
     }
   });
 });

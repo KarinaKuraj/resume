2e322e26d4c207ec8597b8a78cb7e527fc71b823
karinakuraj <karina_gretta@ukr.net>
2016-04-09 09:29:14 +0000

    add new file

--- site.js
+++ site.js
@@ -1,12 +1,71 @@
 /*global $ */
-$(document).ready(function() {
+$/*(document).ready(function() {
   $('#btn1').click(function() {
-     if ($('#about').css('display')=='none') {
-    $('#about').animate({left:'show'},200);
-  }else{
-    $('#about').animate({left:'hide'},200);
-    }
-  });
+    if (document.getElementById('firstpage')) {
+
+                if (document.getElementById('firstpage').style.display == 'none') {
+                    document.getElementById('firstpage').style.display = 'block';
+                    document.getElementById('about').style.display = 'none';
+                }
+                else {
+                    document.getElementById('firstpage').style.display = 'none';
+                    document.getElementById('about').style.display = 'block';
+                }
+            }
 });
+});
+ $('#btn2').click(function() {
+    if (document.getElementById('firstpage')) {
+
+                if (document.getElementById('firstpage').style.display == 'none') {
+                    document.getElementById('firstpage').style.display = 'block';
+                    document.getElementById('thirdpage').style.display = 'none';
+                }
+                else {
+                    document.getElementById('firstpage').style.display = 'none';
+                    document.getElementById('thirdpage').style.display = 'block';
+                }
+            }
+});
+*/
+$(document).ready(function() {
+    $('#btn1').click(function() {
+      $('#about').show('slow');
+        $("#firstpage").hide('slow');
+         $('#thirdpage').hide('slow');
+        
+});
+    $('#btn2').click(function() {
+      $('#thirdpage').show('slow');
+        $("#firstpage").hide('slow');
+          $('#about').hide('slow');
+          
     
     
+});
+
+    $('#btn3').click(function() {
+      $("contact").show('slow');
+        $('#thirdpage').hide('slow');
+          $("#firstpage").hide('slow');
+            $('#about').hide('slow');
+          
+    });
+    
+    
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
